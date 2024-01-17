e809f41427692715770ad3d0717a276a240879a8
Jorge Oscanoa <joscanoav@gmail.com>
2024-01-17 10:37:21 +0100

    lol

--- ActividadClinicaCitas/src/GestionCitas/Cita.java
+++ ActividadClinicaCitas/src/GestionCitas/Cita.java
@@ -4,36 +4,41 @@ public class Cita {
     private String dni;
     private int franja;
     private int tipo;
-    private int hueco;
+    private int hora;
+    private int minutos;
 
-    public Cita(String dni, int franja, int tipo, int hueco) {
-        this.dni = dni;
-        this.franja = franja;
-        this.tipo = tipo;
-        this.hueco = hueco;
-    }
-
+    public Cita(String dni, int franja, int tipo, int hora, int minutos) {
+		this.dni = dni;
+		this.franja = franja;
+		this.tipo = tipo;
+		this.hora = hora;
+		this.minutos = minutos;
+	}
+    
     public String getDni() {
-        return dni;
-    }
+		return dni;
+	}
 
-    public int getFranja() {
-        return franja;
-    }
+	public int getFranja() {
+		return franja;
+	}
 
-    public int getTipo() {
-        return tipo;
-    }
+	public int getTipo() {
+		return tipo;
+	}
 
-    public int getHueco() {
-        return hueco;
-    }
+	public int getHora() {
+		return hora;
+	}
 
-    @Override
+	public int getMinutos() {
+		return minutos;
+	}
+	@Override
     public String toString() {
         return "DNI: " + dni +
                 "\nFranja horaria: " + (franja == 0 ? "Mañana" : "Tarde") +
                 "\nTipo de cita: " + (tipo == 0 ? "Revisión" : "Tratamiento") +
-                "\nHueco: " + (hueco + 1);
+                "\nHueco: " + String.format("%02d:%02d", hora,minutos);
     }
 }
--- ActividadClinicaCitas/src/GestionCitas/Clinica.java
+++ ActividadClinicaCitas/src/GestionCitas/Clinica.java
@@ -13,9 +13,11 @@ public class Clinica {
         citas = new Cita[NUM_CONSULTAS][2][NUM_HUECOS_POR_FRANJA];
     }
 
-    public void solicitarCita(String dni, int tipo, int franja, int hueco) {
+    public void solicitarCita(String dni, int tipo,int franja, int hueco ) {
         if (citas[franja][tipo][hueco] == null) {
-            citas[franja][tipo][hueco] = new Cita(dni, franja, tipo, hueco);
+        	int hora = (franja == 0) ? 9:16;
+        	int minutos = hueco % 2 == 0 ? 0 :30;     	
+        	citas[franja][tipo][hueco] = new Cita (dni,franja,tipo, hora + hueco / 2 , minutos);
             System.out.println("Cita reservada con éxito:");
             System.out.println(citas[franja][tipo][hueco]);
         } else {
@@ -27,8 +29,14 @@ public class Clinica {
                                int nuevaFranja, int nuevoHueco) {
         if (citas[franjaActual][tipoActual][huecoActual] != null) {
             if (citas[nuevaFranja][tipoActual][nuevoHueco] == null) {
+            	
+            	int hora = (nuevaFranja == 0) ? 9:16;
+            	int minutos = nuevoHueco % 2 == 0 ? 0 : 30; 
+            	
                 citas[nuevaFranja][tipoActual][nuevoHueco] = citas[franjaActual][tipoActual][huecoActual];
                 citas[franjaActual][tipoActual][huecoActual] = null;
+                
+      
                 System.out.println("Cita modificada con éxito:");
                 System.out.println(citas[nuevaFranja][tipoActual][nuevoHueco]);
             } else {
@@ -41,10 +49,19 @@ public class Clinica {
 
     public void mostrarHuecosDisponibles(int franja, int tipo) {
         System.out.println("Huecos disponibles:");
+        
+        int inicioHora = (franja == 0)? 9:16;
+        int finHora = (franja == 0) ? 14:21;
 
         for (int i = 0; i < NUM_HUECOS_POR_FRANJA; i++) {
             if (citas[franja][tipo][i] == null) {
-                System.out.println((i + 1) + ". Hueco " + (i + 1));
+                int hora = inicioHora + i /2 ;
+                int minutos = (i % 2 == 0) ? 0: 30;
+                String horaInicio = String.format("%02d:%02d", hora, minutos);
+                String horaFin = String.format("%02d:%02d", hora, minutos);
+            	
+            	
+            	System.out.println((i + 1) + ".  " + "(" + horaInicio + " - " + horaFin + ")") ;
             }
         }
     }
