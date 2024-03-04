ba8eceda469f43f3d552b7bb9df934229504baf8
joscanoav <joscanoav@gmail.com>
2024-03-03 19:39:27 -0500

    lol

--- Evaluacion02/src/ModeloA/Ejercicio02.java
+++ Evaluacion02/src/ModeloA/Ejercicio02.java
@@ -1,6 +1,7 @@
 package ModeloA;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -38,7 +39,44 @@ public class Ejercicio02 {
 			
 		}
 		
+		
+		//Contar elementos de una matriz cadena
+		
+		int numeroElementos = 0;
+		for (int i = 0; i < matriz.length; i++) {
+			numeroElementos++;
+			
+		}
+		
+		int ElementosMatriz = matriz.length;
+		System.out.println(ElementosMatriz);
+		System.out.println("La matriz tiene: " + numeroElementos + " Elementos" );
+		
+		String[][] resultado = new String[matriz.length][matriz[0].length];
+		
+		for (int i = 0; i < matriz.length; i++) {
+		      for (int j = 0; j < matriz[i].length; j++) {
+		    	  
+		    	  // Verificar si es impar
+		    	  
+		    	  if (matriz[i][j].length() % 2 ==1) {
+		    		  //Imprimir X
+		    		  resultado[i][j] = "X";
+		    	  }else {
+		    		  resultado[i][j] = String.valueOf(matriz[i][j].length());		    	  }
+		}
+		
+		
 	}
-	}
+		
+		for (int i = 0; i < resultado.length; i++) {
+            for (int j = 0; j < resultado[i].length; j++) {
+                System.out.print(resultado[i][j] + " ");
+            }
+            // Salto de línea después de cada fila
+            System.out.println();
+        }
+    }
+}
 
   
