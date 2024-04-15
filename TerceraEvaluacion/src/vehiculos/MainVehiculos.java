f0f093a73cb61acdeeda535fb9fd29233160c9eb
Jorge Oscanoa <joscanoav@gmail.com>
2024-04-15 14:03:26 +0200

    lol

--- TerceraEvaluacion/src/vehiculos/MainVehiculos.java
+++ TerceraEvaluacion/src/vehiculos/MainVehiculos.java
@@ -3,8 +3,8 @@ package vehiculos;
 public class MainVehiculos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Vehiculo v1 = new Vehiculo("12346ABC", 4 , 5 , false, 50);
+		Camion c1 = new Camion("5678XXX", 6, 3, true, 800, 30 , 65.4);
 	}
 
 }
